e89b7188e0803d009f6fe462d25e796a28641536
hotdog1004 <hotdog87675210@gmail.com>
2023-11-15 00:10:34 +0900

    경로탐색(DFS-인접행렬:노드개수가 적을 때)

---
+++ graphAndSearch/pathSearchWithAdjacencyMatrix.js
@@ -0,0 +1,57 @@
+/**
+ * 경로탐색(DFS-인접행렬:노드개수가 적을 때)
+ * @param {number} k
+ * @param {number[]} arr
+ * @param {number} m
+ * @return {number}
+ * 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성하세요.
+ * 아래 그래프에서 1번 정점에서 5번 정점으로 가는 가지 수는
+ * 1 2 3 4 5
+ * 1 2 5
+ * 1 3 4 2 5
+ * 1 3 4 5
+ * 1 4 2 5
+ * 1 4 5
+ * 총 6가지입니다.
+ */
+
+function solution(n, arr) {
+  let answer = 0;
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  let graph = Array.from({ length: n + 1 }, () =>
+    Array.from({ length: n + 1 }).fill(0)
+  );
+
+  for (let [a, b] of arr) {
+    graph[a][b] = 1;
+  }
+  const DFS = (v) => {
+    if (v === n) answer++;
+    else {
+      for (let i = 1; i <= n; i++) {
+        if (graph[v][i] === 1 && ch[i] === 0) {
+          ch[i] = 1;
+          DFS(v + 1);
+          ch[i] = 0;
+        }
+      }
+    }
+  };
+  ch[1] = 1;
+  DFS(1);
+
+  return answer;
+}
+
+let arr = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+];
+console.log(solution(5, arr));
